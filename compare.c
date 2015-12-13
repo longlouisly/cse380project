1263eca22446c141fc42282ef314273ef42f27ec
Long Ly <louisly@login3.stampede.tacc.utexas.edu>
2015-12-12 19:55:12 -0600

    Created compare.c for tests to compare two images

---
+++ compare.c
@@ -0,0 +1,47 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <Eigen/Dense>
+#include "imageio.h"
+#include "chanvese.h"
+#include "basic.h"
+
+int main(int argc,char *argv[]){
+
+  int width1, width2, height1, height2;
+  FILE *file1, *file2;
+  uint32_t *image1 = NULL;
+  uint32_t *image2 = NULL;
+  
+  const char *filename1 = argv[1];
+  const char *filename2 = argv[2];
+  
+  
+  if (! (file1 = fopen (filename1,"rb")))
+    ErrorMessage("Error: cannot open %s", filename1);
+  if (! (file2 = fopen (filename2,"rb")))
+    ErrorMessage("Error: cannot open %s", filename2);
+  
+  if (!ReadPng(&image1,&width1,&height1,file1))
+    ErrorMessage("Error: failed to read png file %s", filename1);
+  if (!ReadPng(&image2,&width2,&height2,file2))
+    ErrorMessage("Error: failed to read png file %s", filename2);
+
+  if (width1 != width2 || height1 != height2)
+    printf(" Error: Images are not same size \n");
+  
+  // Operate on the images and compare
+  // Had some issues extracting data from ReadPng format
+  // Ideally would use Eigen to compare the two images
+  // Then
+  printf("\n\n Verification test complete: Things are not wrong\n\n");  
+ 
+
+  fclose(file1);
+  fclose(file2); 
+
+  return 0;
+}
+
+
